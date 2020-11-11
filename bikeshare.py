bcba323734d5ec0bf2b04bc0430ea12d43cd3c5c
anuarora <anu.arora@quantumblack.com>
2020-11-11 23:08:12 +0000

    Revert "docs: Update README.md description and doc string"
    This reverts commit 9cda8861a8912277e9d0757380eb4f881eeab091.

--- bikeshare.py
+++ bikeshare.py
@@ -46,11 +46,7 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel.
-
-    Returns:
-        None
-    """
+    """Displays statistics on the most frequent times of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
