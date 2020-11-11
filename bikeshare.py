c15971dfb8a4a4c262ba77d0c6a8f9207e92b33b
anuarora <anu.arora@quantumblack.com>
2020-11-11 23:10:06 +0000

    docs: Update doc string to return none

--- bikeshare.py
+++ bikeshare.py
@@ -46,7 +46,11 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """Displays statistics on the most frequent times of travel.
+
+    Returns:
+        None
+    """
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
